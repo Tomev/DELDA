1fbb57245cc83dce654de242db744176b415d486
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-08-10 00:09:28 +0200

    DELDA class added.

---
+++ src/DELDA.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tomev on 10.08.2018.
+//
+
+#include "DELDA.h"
---
+++ src/DELDA.h
@@ -0,0 +1,13 @@
+#ifndef DELDA_DELDA_H
+#define DELDA_DELDA_H
+
+/***
+ *  DELDA (Density Estimator for Large DAta Sets), is a class describing an algorithm that would perform density
+ *  estimation, using kernel density estimator, on a large data sets.
+ */
+class DELDA {
+
+};
+
+
+#endif //DELDA_DELDA_H
